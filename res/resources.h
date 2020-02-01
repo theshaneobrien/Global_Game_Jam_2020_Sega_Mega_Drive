62805ca83e18ae79136bc34207b0d718df108027
Shane <shane@supermegabyte.com>
2020-02-01 12:00:15 +0000

    prep projectile Added in shield appearing  / disappearing Fixed Jump Bug

--- res/resources.h
+++ res/resources.h
@@ -3,6 +3,8 @@
 
 extern const u8 music[15872];
 extern const SpriteDefinition player1Sprite;
+extern const SpriteDefinition shieldSprite;
+extern const SpriteDefinition projectileSprite;
 extern const Image BGBuildings;
 extern const Image BGClouds;
 
