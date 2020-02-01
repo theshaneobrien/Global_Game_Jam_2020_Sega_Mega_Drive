587f455ad79e18dc3268b18e0f88fb29129c4027
Terracorrupt <podge-2006@hotmail.com>
2020-02-01 10:45:37 +0000

    feat(ggj): add music for testing

--- main.c
+++ main.c
@@ -39,6 +39,7 @@ void setupPlayers();
 void gravity();
 void playerJumping();
 void setPlayerPosition();
+void setupMusic();
 
 //Button Functions
 int p1ButtonPressEvent(int button);
@@ -82,6 +83,19 @@ void init()
 	SPR_init(0, 0, 0);
 	setupPlayField();
 	setupPlayers();
+	setupMusic();
+}
+
+void setupMusic()
+{
+	XGM_setLoopNumber(10);
+	//Load PCM 65 with the start sound effect, how long it is
+	//XGM_setPCM(65, &start, 17664);
+	//Play PCM 65, Priority 1 signal from starflet, sound channel 2
+	//XGM_startPlayPCM(65, 1, SOUND_PCM_CH2);
+	//Play our Music
+	XGM_startPlay(&music);
+
 }
 
 void setupPlayField()
--- res/resources.res
+++ res/resources.res
@@ -6,6 +6,6 @@ IMAGE devBG "tilemaps\BG_Placeholder_Coloured.png" 0
 //Sprites
 SPRITE player1Sprite "sprites\Robot_Test2_64.png" 8 8 0 15
 //Music
-
+XGM music "sound/ggj2020.vgm"
 
 //Sound
